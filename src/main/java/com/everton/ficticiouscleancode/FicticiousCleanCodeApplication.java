a19364498959ea98ca03ef46e074f6168d85232d
Everton Soethe <evertonhsoethe@gmail.com>
2022-01-31 00:35:51 -0300

    Commit inicial  - Adicionado dependencias do projeto

---
+++ src/main/java/com/everton/ficticiouscleancode/FicticiousCleanCodeApplication.java
@@ -0,0 +1,13 @@
+package com.everton.ficticiouscleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicticiousCleanCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicticiousCleanCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everton/ficticiouscleancode/FicticiousCleanCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everton.ficticiouscleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicticiousCleanCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
